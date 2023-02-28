4bb7d08753a0bd1d400a20c89351c14bdb802e79
Marine Morales <64949883+MarineMorales@users.noreply.github.com>
2023-03-01 10:49:29 +1100

    Update GuessTheNumber.py

--- GuessTheNumber.py
+++ GuessTheNumber.py
@@ -1,4 +1,11 @@
-#Guess the Number game
+#Game: Guess the Number
+
+#Rules: 
+#The computer will think of a secret number from 1 to 20 and ask the user to guess it. 
+#After each guess, the computer will tell the user whether the number is too high or too low.
+#The user wins if they can guess the number within six tries.
+
+#Program:
 import random #Import module "random"
 
 guessesTaken = 0
