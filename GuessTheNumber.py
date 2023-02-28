a675b5204ad26d8b51292b70562064b3793e418c
Marine Morales <64949883+MarineMorales@users.noreply.github.com>
2023-03-01 10:45:40 +1100

    Update GuessTheNumber.py

--- GuessTheNumber.py
+++ GuessTheNumber.py
@@ -12,8 +12,10 @@ for guessesTaken in range(6):
 	print('Take a guess.')
 	guess = input()
 	guess = int(guess)
+	
+# Do not confuse the boolean comparison operator "==" (True vs False output) with the assignment operator "=".
 
-	if guess < number:
+	if guess < number: # This is a boolean data comparison
 		print('Your guess is too low.')
 
 	if guess > number:
